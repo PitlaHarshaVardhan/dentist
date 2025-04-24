da72a2307322ea6af079ac817ae6f20901dc6476
harshavardhanpitla <163458889+harsha665@users.noreply.github.com>
2025-04-24 23:42:48 +0530

    react url added

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 const port = 3001;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://dentist-1.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
